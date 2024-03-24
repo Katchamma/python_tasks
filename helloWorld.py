e2e08acb9849ab4a8ee1004528f9f5144bffef56
Kat Chamma <katchamma@outlook.com>
2024-03-24 19:43:10 +0000

    Created the prompt asking for folder path for issue #1

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,7 @@
 user_name = input("Enter your name: ")
 project_name = input("Enter your project name: ")
+folder_path = input("Enter the path to get files: ")
+
 print(f"Hello {user_name}")
 print(f"Your project name is: {project_name}")
 print("Git is awsome!")
