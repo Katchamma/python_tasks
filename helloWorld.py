7b00bd9121618be01f3e2d5ef8e65e25b948dc1c
Kat Chamma <katchamma@outlook.com>
2024-03-24 20:24:07 +0000

    Added support for listing files for issue #2

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,14 @@
+from os import listdir
+
+
 user_name = input("Enter your name: ")
 project_name = input("Enter your project name: ")
 folder_path = input("Enter the path to get files: ")
+file_names = listdir(folder_path)
 
 print(f"Hello {user_name}")
 print(f"Your project name is: {project_name}")
 print("Git is awsome!")
+print(f"files in folder: {folder_path}:")
+for file_name in file_names:
+    print(file_name)
